cd37f59ddf6de8fd2e583cebc58d637c175180e8
logesh0219 <logesh@madebyfire.com>
2025-01-27 10:27:10 +0530

    header will be changed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -39,85 +39,92 @@ const Header = ({ toggle, toggleTheme }) => {
 
   return (
     <header>
-      <div className="header-container">
-        <div className="header-blk paddingX">
-          <div className="header-logo">
-            <a href="/">
-              {toggle ? (
-                <img
-                  src="https://flexiblog-education.netlify.app/static/bad5f02928a2842d0cf1f649d6f7d1da/edeef/logo-dark.webp"
-                  alt="logo"
-                />
-              ) : (
-                <img
-                  src="https://flexiblog-education.netlify.app/static/e1abc27a01b56d93f5fcdff62c84fdd7/edeef/logo.webp"
-                  alt="logo"
-                />
-              )}
-            </a>
-          </div>
-          <SearchBar />
-          <div className="d-flex align-center">
-            <div className="header-link-container">
-              <div className="header-link-wrapper">
-                <ul className="header-links">
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/tutors">Tutors</Link>
-                  </li>
-                  <li>
-                    <Link to="/contact">Contact</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="responsive-icons">
-              <div
-                className="search-bar-icon icon"
-                onClick={toggleSearchBar}
-              >
-                <i class="bx bx-search"></i>
-              </div>
-              {searchBar && (
-                <div className="search-bar-open">
-                  <div className="search-bar-container">
-                    <IoSearch className="search-icon" />
-                    <input
-                      type="text"
-                      placeholder="Discover news, articles and more..."
-                      className="input-box"
-                    />
-                  </div>
-                  <div className="search-bar-overlay" onClick={toggleSearchBar}></div>
-                  <button onClick={toggleSearchBar} className='search-bar-close-btn'><i class='bx bx-x'></i></button>
-                </div>
-              )}
-              <div className="toggle-icon icon" onClick={toggleTheme}>
+      <div className="container">
+        <div className="header-container">
+          <div className="header-blk paddingX">
+            <div className="header-logo">
+              <a href="/">
                 {toggle ? (
-                  <i class="bx bxs-sun"></i>
+                  <img
+                    src="https://flexiblog-education.netlify.app/static/bad5f02928a2842d0cf1f649d6f7d1da/edeef/logo-dark.webp"
+                    alt="logo"
+                  />
                 ) : (
-                  <i class="bx bxs-moon"></i>
+                  <img
+                    src="https://flexiblog-education.netlify.app/static/e1abc27a01b56d93f5fcdff62c84fdd7/edeef/logo.webp"
+                    alt="logo"
+                  />
                 )}
-              </div>
-              <div className="menu-bar icon" onClick={toggleMenu}>
-                <i class="bx bx-menu"></i>
-              </div>
+              </a>
             </div>
-            <ul
-              className={`toggle-switch ${toggle ? 'active' : ''}`}
-              onClick={toggleTheme}
-            >
-              <li>
-                {' '}
-                <FaSun className="sun" />{' '}
-              </li>
-              <li>
-                {' '}
-                <FaMoon className="moon" />{' '}
-              </li>
-            </ul>
+            <SearchBar />
+            <div className="d-flex align-center">
+              <div className="header-link-container">
+                <div className="header-link-wrapper">
+                  <ul className="header-links">
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/tutors">Tutors</Link>
+                    </li>
+                    <li>
+                      <Link to="/contact">Contact</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="responsive-icons">
+                <div className="search-bar-icon icon" onClick={toggleSearchBar}>
+                  <i class="bx bx-search"></i>
+                </div>
+                {searchBar && (
+                  <div className="search-bar-open">
+                    <div className="search-bar-container">
+                      <IoSearch className="search-icon" />
+                      <input
+                        type="text"
+                        placeholder="Discover news, articles and more..."
+                        className="input-box"
+                      />
+                    </div>
+                    <div
+                      className="search-bar-overlay"
+                      onClick={toggleSearchBar}
+                    ></div>
+                    <button
+                      onClick={toggleSearchBar}
+                      className="search-bar-close-btn"
+                    >
+                      <i class="bx bx-x"></i>
+                    </button>
+                  </div>
+                )}
+                <div className="toggle-icon icon" onClick={toggleTheme}>
+                  {toggle ? (
+                    <i class="bx bxs-sun"></i>
+                  ) : (
+                    <i class="bx bxs-moon"></i>
+                  )}
+                </div>
+                <div className="menu-bar icon" onClick={toggleMenu}>
+                  <i class="bx bx-menu"></i>
+                </div>
+              </div>
+              <ul
+                className={`toggle-switch ${toggle ? 'active' : ''}`}
+                onClick={toggleTheme}
+              >
+                <li>
+                  {' '}
+                  <FaSun className="sun" />{' '}
+                </li>
+                <li>
+                  {' '}
+                  <FaMoon className="moon" />{' '}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
